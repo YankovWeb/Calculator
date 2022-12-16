227fc06adacb7942af690090d4058e43921c7f0e
Grigor Yankov <yankovweb@outlook.com>
2022-12-16 20:13:09 +0200

    Css Ready

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
+import "./style.css";
+
 function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">123,123 *</div>
+        <div className="current-operand">123,123</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
